37c91f20b5dd842ededeea33ec324b249d9422aa
gostfather <857902620@qq.com>
2020-06-05 00:28:59 +0800

    fix path

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,9 +7,8 @@ describe('webpack.base.js test case', () => {
     console.log(baseConfig);
     
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/shenjinyong/Desktop/webpackbuild/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/shenjinyong/Desktop/webpackbuild/test/smoke/template/src/search/index.js');
-        
+        assert.equal(baseConfig.entry.index.indexOf('/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js') > -1, true);
     })
     
 })
